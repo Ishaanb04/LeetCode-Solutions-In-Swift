cb3eb27ad598998b2b2e918bc9b2f11b556bc9f8
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2024-06-07 16:45:32 -0700

    Added 560. Subarray Sum Equals K

---
+++ LeetCode.playground/Pages/560. Subarray Sum Equals K.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+// 560. Subarray Sum Equals K
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+class Solution {
+    // Brute-Force Solution- O(n^2)
+    func subarraySum(_ nums: [Int], _ k: Int) -> Int {
+        var sum = 0
+        var count = 0
+        for i in 0 ..< nums.count {
+            sum = 0
+            for j in i ..< nums.count {
+                sum += nums[j]
+                if sum == k {
+                    count += 1
+                }
+            }
+        }
+        return count
+    }
+    
+    // Prefix Sum using hMap
+    func subarraySum(_ nums: [Int], _ k: Int) -> Int {
+        var prefixCount = [Int: Int]()
+        var sum = 0
+        var result = 0
+        prefixCount[0] = 1
+        
+        for num in nums {
+            sum += num
+            
+            if let count = prefixCount[sum - k] {
+                result += count
+            }
+            prefixCount[sum, default: 0] += 1
+        }
+        return result
+    }
+}
