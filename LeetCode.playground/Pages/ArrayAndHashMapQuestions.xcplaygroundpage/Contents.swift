3e645ea09663b4994ad5bb01f4a2516828352591
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2024-06-06 17:44:05 -0700

    Added 912. Sort an Array

---
+++ LeetCode.playground/Pages/ArrayAndHashMapQuestions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+class Solution {
+    // 912. Sort an Array
+    // https://leetcode.com/problems/sort-an-array/description/
+    // MergeSort - O(nLogn) Time
+    func sortArray(_ nums: [Int]) -> [Int] {
+        func sortArray(_ nums: [Int]) -> [Int] {
+            guard nums.count > 1 else { return nums }
+                
+            // Merge Sequence of two sorted parts
+            func mergeTwoSortedHalves(_ leftHalf: [Int], _ rightHalf: [Int]) -> [Int] {
+                var sortedArray = [Int]()
+                    
+                var index1 = 0
+                var index2 = 0
+                    
+                while index1 < leftHalf.count, index2 < rightHalf.count {
+                    if leftHalf[index1] < rightHalf[index2] {
+                        sortedArray.append(leftHalf[index1])
+                        index1 += 1
+                    } else {
+                        sortedArray.append(rightHalf[index2])
+                        index2 += 1
+                    }
+                }
+                    
+                if index1 < leftHalf.count {
+                    sortedArray.append(contentsOf: leftHalf[index1...])
+                } else {
+                    sortedArray.append(contentsOf: rightHalf[index2...])
+                }
+                return sortedArray
+            }
+                
+            // Recursive fuction
+            func mergeSort(_ nums: [Int]) -> [Int] {
+                guard nums.count > 1 else { return nums }
+                let midIndex = nums.count / 2
+                let left = mergeSort(Array(nums[0..<midIndex]))
+                let right = mergeSort(Array(nums[midIndex..<nums.count]))
+                    
+                return mergeTwoSortedHalves(left, right)
+            }
+            return mergeSort(nums)
+        }
+    }
+}
---
+++ LeetCode.playground/Pages/IntroPage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+/*
+    LeetCode Solutions in Swift
+
+    Welcome to the LeetCode Solutions Playground! This playground contains a collection of solutions to popular LeetCode problems, implemented in Swift. It is designed to help you practice and improve your algorithm and problem-solving skills using Swift.
+ */
