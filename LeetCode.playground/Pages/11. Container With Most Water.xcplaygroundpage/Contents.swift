a17c7349b93b29d7c3b123d25f5740b1b142e756
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2024-06-16 09:55:08 -0700

    Added 11. Container With Most Water

---
+++ LeetCode.playground/Pages/11. Container With Most Water.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+// 11. Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/description/
+
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var start = 0
+        var end = height.count - 1
+        var maxArea = Int.min
+        while start < end {
+            let currArea = min(height[start], height[end]) * (end - start)
+            maxArea = max(maxArea, currArea)
+            if height[start] < height[end] {
+                start += 1
+            } else {
+                end -= 1
+            }
+        }
+        return maxArea
+    }
+}
