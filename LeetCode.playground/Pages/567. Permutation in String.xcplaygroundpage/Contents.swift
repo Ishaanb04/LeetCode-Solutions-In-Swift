6027dcbe5e0ba3514f1571fbfab4ddb727de97a9
Ishaan Bhasin <ishaanbhasin@Ishaans-Mac-Studio.local>
2024-06-19 13:26:37 -0700

    Added 567. Permutation in String

--- LeetCode.playground/Pages/567. Permutation in String.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/567. Permutation in String.xcplaygroundpage/Contents.swift
@@ -3,7 +3,34 @@
 
 class Solution {
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
-       guard s1.count <= s2.count else { return false }
+        guard s1.count <= s2.count else { return false }
+        var S1 = Array(s1)
+        var S2 = Array(s2)
+        var freqS1 = Array(repeating: 0, count: 26)
+        var freqS2 = Array(repeating: 0, count: 26)
+
+        for i in 0 ..< S1.count {
+            var index = Int(S1[i].asciiValue! - Character("a").asciiValue!)
+            freqS1[index] += 1
+        }
+        var start = 0
+        for end in 0 ..< S2.count {
+            
+            if end - start == S1.count {
+                var index = Int(S2[start].asciiValue! - Character("a").asciiValue!)
+                freqS2[index] -= 1
+                start += 1
+            }
+            
+            var index = Int(S2[end].asciiValue! - Character("a").asciiValue!)
+            freqS2[index] += 1
+            if freqS1 == freqS2 { return true }
+        }
+        return false
+    }
+
+    func checkInclusion2(_ s1: String, _ s2: String) -> Bool {
+        guard s1.count <= s2.count else { return false }
         var sortedS1 = Array(s1).sorted()
         var S2 = Array(s2)
         var lenS1 = sortedS1.count
@@ -15,3 +42,6 @@ class Solution {
         return false
     }
 }
+
+var s = Solution()
+s.checkInclusion("ab", "eidbaooo")
