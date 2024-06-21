3656a41f84269075c92cfdae8f0dc4acd60dc6ff
Ishaan Bhasin <ishaanbhasin@Ishaans-Mac-Studio.local>
2024-06-21 07:19:54 -0700

    Added 704. Binary Search

---
+++ LeetCode.playground/Pages/704. Binary Search.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+// 704. Binary Search
+// https://leetcode.com/problems/binary-search/description/
+
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+
+        while left <= right {
+            var mid = left + (right - left) / 2
+
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] < target {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        return -1
+    }
+}
