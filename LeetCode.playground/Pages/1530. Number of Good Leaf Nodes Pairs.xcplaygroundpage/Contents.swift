b8b6201dad79ccba2f246e2077e0c9962c947f15
Ishaan <ishaanb04@gmail.com>
2024-07-13 04:49:31 -0700

    Added 1530. Number of Good Leaf Nodes Pairs

--- LeetCode.playground/Pages/1530. Number of Good Leaf Nodes Pairs.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/1530. Number of Good Leaf Nodes Pairs.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
+// 1530. Number of Good Leaf Nodes Pairs
+// https://leetcode.com/problems/number-of-good-leaf-nodes-pairs/description/
+class Solution {
+    func countPairs(_ root: TreeNode?, _ distance: Int) -> Int {
+        var answer = 0
+        func helper(_ node: TreeNode?) -> [Int] {
+            guard let node = node else { return [] }
 
-import Foundation
+            if node.left == nil, node.right == nil { return [1] }
 
-var greeting = "Hello, playground"
+            var leftNodes = helper(node.left)
+            var rigthNodes = helper(node.right)
 
-//: [Next](@next)
+            for left in leftNodes {
+                for right in rigthNodes {
+                    if left + right <= distance { answer += 1 }
+                }
+            }
+            return Array(leftNodes.map { $0 + 1 }) + Array(rigthNodes.map { $0 + 1 })
+        }
+        helper(root)
+        return answer
+    }
+}
